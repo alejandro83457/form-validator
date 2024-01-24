bce4d0dcdacbd4ff97ead4e64dbdc3eca8f7f601
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-24 00:25:15 -0800

    Add validity functionality to email, zip code, password, and confirm password

--- src/index.js
+++ src/index.js
@@ -11,4 +11,47 @@ const generateDropDown = () => {
   });
 };
 
+// check if both passwords match
+const checkPasswords = () => {
+  const password = document.querySelector('#password');
+  const confirm = document.querySelector('#confirm');
+
+  confirm.addEventListener('input', () => {
+    if (password.value !== confirm.value) {
+      confirm.setCustomValidity('Passwords need to match!');
+      password.setAttribute('class', 'error');
+      confirm.setAttribute('class', 'error');
+    } else {
+      confirm.setCustomValidity('');
+      password.removeAttribute('class', 'error');
+      confirm.removeAttribute('class', 'error');
+    }
+  });
+};
+
+const isValid = (el) => {
+  if (!el.checkValidity()) {
+    el.setAttribute('class', 'error');
+  } else {
+    el.removeAttribute('class', 'error');
+  }
+};
+
+const validate = () => {
+  const submit = document.querySelector('#submit');
+  const email = document.querySelector('#email');
+  const zip = document.querySelector('#zip');
+  const password = document.querySelector('#password');
+  const confirm = document.querySelector('#confirm');
+
+  submit.addEventListener('click', () => {
+    isValid(email);
+    isValid(zip);
+    isValid(password);
+    isValid(confirm);
+  });
+};
+
 generateDropDown();
+checkPasswords();
+validate();
