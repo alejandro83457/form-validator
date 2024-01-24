676a6cda903ee810e665e9523f88ddc550ee2e0d
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-24 00:54:17 -0800

    Complete invalid styling on form and populate congrats div on form submission

--- src/index.js
+++ src/index.js
@@ -11,6 +11,58 @@ const generateDropDown = () => {
   });
 };
 
+// checks if email is set up properly
+const checkEmail = () => {
+  const email = document.querySelector('#email');
+  email.addEventListener('input', () => {
+    if (!email.checkValidity()) {
+      email.setAttribute('class', 'error');
+      email.reportValidity();
+    } else {
+      email.removeAttribute('class', 'error');
+    }
+  });
+};
+
+// will check only on form submission
+const checkCountryOnce = () => {
+  const country = document.querySelector('#country');
+  if (country.value === 'Select country') {
+    country.setCustomValidity('You must choose a country!');
+    country.setAttribute('class', 'error');
+  } else {
+    country.setCustomValidity('');
+    country.removeAttribute('class', 'error');
+  }
+};
+
+// will keep checking after incorrect form submission
+const checkCountry = () => {
+  const country = document.querySelector('#country');
+  country.addEventListener('change', () => {
+    if (country.value === 'Select country') {
+      country.setCustomValidity('You must choose a country!');
+      country.setAttribute('class', 'error');
+      country.reportValidity();
+    } else {
+      country.setCustomValidity('');
+      country.removeAttribute('class', 'error');
+    }
+  });
+};
+
+const checkZIP = () => {
+  const zip = document.querySelector('#zip');
+  zip.addEventListener('input', () => {
+    if (!zip.checkValidity()) {
+      zip.setAttribute('class', 'error');
+      zip.reportValidity();
+    } else {
+      zip.removeAttribute('class', 'error');
+    }
+  });
+};
+
 // check if both passwords match
 const checkPasswords = () => {
   const password = document.querySelector('#password');
@@ -21,6 +73,7 @@ const checkPasswords = () => {
       confirm.setCustomValidity('Passwords need to match!');
       password.setAttribute('class', 'error');
       confirm.setAttribute('class', 'error');
+      confirm.reportValidity();
     } else {
       confirm.setCustomValidity('');
       password.removeAttribute('class', 'error');
@@ -29,6 +82,7 @@ const checkPasswords = () => {
   });
 };
 
+// template function to check if an element is valid
 const isValid = (el) => {
   if (!el.checkValidity()) {
     el.setAttribute('class', 'error');
@@ -37,6 +91,7 @@ const isValid = (el) => {
   }
 };
 
+// will check if all input elements are valid or not.
 const validate = () => {
   const submit = document.querySelector('#submit');
   const email = document.querySelector('#email');
@@ -46,12 +101,28 @@ const validate = () => {
 
   submit.addEventListener('click', () => {
     isValid(email);
+    checkCountryOnce();
     isValid(zip);
     isValid(password);
     isValid(confirm);
   });
 };
 
+const submitForm = () => {
+  const form = document.querySelector('form');
+  const congrats = document.querySelector('#congrats');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    congrats.textContent = '👏🏻';
+  });
+};
+
+// one-time calls
 generateDropDown();
+checkEmail();
+checkCountry();
+checkZIP();
 checkPasswords();
 validate();
+submitForm();
